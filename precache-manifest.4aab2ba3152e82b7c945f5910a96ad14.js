71c89dac62fcba12cf46c5b9473bc3e1d2774594
Jung Hong <junghong0512@gmail.com>
2020-03-10 00:30:33 +0900

    Updates

---
+++ precache-manifest.4aab2ba3152e82b7c945f5910a96ad14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461f1956c44012524b8c6a30e5bc84f8",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f159e26d5a7fd4942c77",
+    "url": "/movie_app_2020/static/css/main.ff93110e.chunk.css"
+  },
+  {
+    "revision": "ee5f40fffa95eb75bff2",
+    "url": "/movie_app_2020/static/js/2.41708d2b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.41708d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f159e26d5a7fd4942c77",
+    "url": "/movie_app_2020/static/js/main.793cf73b.chunk.js"
+  },
+  {
+    "revision": "5b9de689d76c950937df",
+    "url": "/movie_app_2020/static/js/runtime-main.db2dcdc9.js"
+  }
+]);
