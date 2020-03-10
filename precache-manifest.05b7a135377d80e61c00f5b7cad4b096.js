1666d898dfcc4451be1ec1dac107563fea2453c2
Jung Hong <junghong0512@gmail.com>
2020-03-10 19:09:45 +0900

    Updates

--- precache-manifest.05b7a135377d80e61c00f5b7cad4b096.js
+++ precache-manifest.05b7a135377d80e61c00f5b7cad4b096.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7362a20f8e3a343e0748cbeab59d2a08",
+    "revision": "2050c0d33cb4a28468d25a73ecf4e7d5",
     "url": "/movie_app_2020/index.html"
   },
   {
-    "revision": "8c18c8458d735ae9f26c",
-    "url": "/movie_app_2020/static/css/main.1b2e4bb4.chunk.css"
+    "revision": "8b8201ebe7171d6ff9e4",
+    "url": "/movie_app_2020/static/css/main.a294e67c.chunk.css"
   },
   {
     "revision": "cf4ff15a7ce3697f3c48",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie_app_2020/static/js/2.7bb55e1a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8c18c8458d735ae9f26c",
-    "url": "/movie_app_2020/static/js/main.e27b6a42.chunk.js"
+    "revision": "8b8201ebe7171d6ff9e4",
+    "url": "/movie_app_2020/static/js/main.36ddc123.chunk.js"
   },
   {
     "revision": "5b9de689d76c950937df",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2020/precache-manifest.e26de8d619029c1605302ed2a2713b7f.js"
+  "/movie_app_2020/precache-manifest.05b7a135377d80e61c00f5b7cad4b096.js"
 );
 
 self.addEventListener('message', (event) => {
