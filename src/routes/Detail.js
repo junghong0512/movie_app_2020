03b4dde09b307d69426cbc2b8211df8a6884933a
Jung Hong <junghong0512@gmail.com>
2020-03-10 19:08:55 +0900

    CSS Updates and Detail Updates

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 // function Detail(props) {
 //   console.log(props);
@@ -14,8 +15,30 @@ class Detail extends React.Component {
   }
   render() {
     const { location } = this.props;
+    const { state } = location;
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      return (
+        <div className="detail">
+          <img
+            className="detail__image"
+            src={state.poster}
+            title={state.title}
+            alt={state.title}
+          />
+          <div className="detail__data">
+            <h3 className="detail__title">{state.title}</h3>
+            <h5 className="detail__year">{state.year}</h5>
+            <ul className="detail__genres">
+              {state.genres.map((genre, index) => (
+                <li key={index} className="genres__genre">
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className="detail__summary">{state.summary}</p>
+          </div>
+        </div>
+      );
     } else {
       return null;
     }
